f29220a66d5a0f822c872177a6dd201601c41dff
DiciembreBW <diciembrebw@gmail.com>
2018-11-15 02:52:26 +0700

    ทำ input component

---
+++ components/form/input.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <label>
+      <slot>Label Input</slot>
+    </label>
+    <!-- pass direct -->
+    <!-- <input type="text" @input="$emit('input', $event.target.value)"> -->
+
+    <!-- pass watch -->
+    <input type="text" v-model="text">
+
+    <button @click="clear" v-if="text">x</button>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        text: ''
+      }
+    },
+
+    methods: {
+      // methods
+    //   clear value
+        clear: function () {
+            this.text = ''
+        }
+    },
+
+    watch: {
+      // watch
+      text: function (value) {
+        this.$emit('input', value)
+      }
+    }
+
+  }
+
+</script>
+
+<style scoped>
+  div {
+    padding: 24px 0;
+  }
+
+  div>input {
+    border-radius: 21px;
+    border: 1px solid gray;
+    padding: 4px 12px;
+  }
+
+</style>
--- pages/Brand.vue
+++ pages/Brand.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
-    <h1 class="title is-2">{{test}}</h1>
+    <h1 class="title is-2">{{config.title}}</h1>
+
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, sunt dolore rem quidem ut corporis inventore
       sint, accusantium quia aliquid amet tempore hic quae officiis rerum perferendis magni architecto! Necessitatibus!</p>
-    <!-- <ul>
-      <li v-for="(item, index) in dumb" :key="index">
-        {{item.name}}
-      </li>
-    </ul> -->
+
+    <v-input v-model="user.name"> name </v-input>
+    <v-input v-model="user.lastname"> lastname </v-input>
+    <v-input v-model="user.email"> email </v-input>
+
+    <hr>
+    <pre> {{user}} </pre>
   </div>
 </template>
 
 <script>
+
+import Input from '@/components/form/input.vue'
+
   export default {
+    components: {
+        'v-input': Input
+    },
+
     data: () => {
       return {
-        test: 'Brand...'
+        config: {
+          title: 'Brand Page...'
+        },
+        user: {}
       }
+    },
+
+    methods: {
     }
+
   }
 
 </script>

86b1b2454933f24e8abf116dfd7c44eade0d5d6c
DiciembreBW <diciembrebw@gmail.com>
2018-12-15 22:32:59 +0700

    เริ่มใช้ Plugin

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,42 @@
   <div>
     <h1 class="title is-2">Index page</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ea fuga recusandae quasi minus eveniet quas harum a. In ut labore hic mollitia ad expedita cupiditate delectus temporibus perspiciatis reiciendis.</p>
+    <p>
+      <input type="text" v-model="test">
+      <button @click="add">add</button>
+    </p>
+    <h1 class="title">
+      {{test}}
+    </h1>
   </div>
 </template>
 
 <script>
+
+import {fireDb} from '@/plugins/firebase.js'
+
+// ทดสอบใช้ plugin
+import pl from '@/plugins/med.js'
+// 
+
 export default {
   components: {
   },
+  data() {
+    return {
+      test: 'pass'
+    }
+  },
   asyncData(context) {
-    // use context
-    context.app.test('Hello')
+
+  },
+  methods: {
+  // ใช้ methods จาก plugins
+    add: function (context) {
+      this.$test()
+      // console.log(context)
+    }
+  //
   }
 }
 </script>
--- plugins/med.js
+++ plugins/med.js
@@ -1,3 +1,12 @@
+// file plugin
+
+// inject plugin
+import Vue from 'vue'
+
+Vue.prototype.$test = () => console.log('pass')
+// ใช้ this.$test() ในหน้า index.vue or etc.
+// ใน function methods ก็ได้
+// 
 export default ({app}, inject) => {
 
     app.test= value => console.log('pass + ', value)

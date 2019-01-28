1f97f152d7ccca55e6f4ec28991ed4d9aa83e229
diciembrebw@gmail.com <diciembrebw@gmail.com>
2019-01-28 21:22:05 +0700

    refact item component

---
+++ components/FieldComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+        <!-- <car-component v-model="value.carbrand"/>
+        <cleave-component v-model="value.car.years" :config="{blocks: [4]}" elementName="car-year">ปี (พ.ศ.)</cleave-component>
+        <select-component v-model="value.car.color" :data="Car.color">สีรถ</select-component>
+        <input-component v-model="value.car.cc" >ขนาดเครื่องยนต์</input-component> -->
+    <div class="row"> <button class="button is-info" @click="apply">บันทึก</button></div>
+
+    <hr> 
+    <!-- <h1 class="title is-2">หน้าแรก</h1> -->
+  </div>
+</template>
+
+<script>
+// import components
+import InputComponent from '@/components/form/InputComponent'
+import SelectComponent from '@/components/form/SelectComponent'
+import CleaveComponent from '@/components/form/CleaveComponent'
+import CarComponent from '@/components/CarComponent'
+
+// data
+import InitialData from '@/static/initialData.json'
+import CarData from '@/static/car-list.json'
+
+//firestore
+import {db} from '@/plugins/Firestore'
+
+export default {
+  
+  data: function () {
+    return {}
+  },
+
+  components: {
+    InputComponent,
+    SelectComponent,
+    CleaveComponent,
+    CarComponent
+  },
+
+  methods: {
+  },
+
+  computed: {
+  }
+}
+</script>
--- components/ItemsComponent.vue
+++ components/ItemsComponent.vue
@@ -2,23 +2,29 @@
     <div>
         <div class="card" v-for="(item, index) in data" :key="index" style="margin-bottom: 8px">
         <div class="card-header">
-            <p class="card-header-title">{{item.carbrand.name}} {{item.carbrand.version}} </p>
+            <p class="card-header-title" v-if="item.carbrand">{{item.carbrand.name}} {{item.carbrand.version}} </p>
         </div>
         <div class="card-content">
             <div class="content">
-            <span class="label-item"> <b>ขนาดเครื่องยนต์ </b> {{item.car.cc}} </span>
-            <span class="label-item"> <b>สี</b>  {{item.car.color}}</span>
-            <span class="label-item"> <b>ปีรถ</b>  {{item.car.cc}}</span>
+            <div v-if="item.car">
+                <span class="label-item"> <b>ขนาดเครื่องยนต์ </b> {{item.car.cc}} </span>
+                <span class="label-item"> <b>สี</b>  {{item.car.color}}</span>
+                <span class="label-item"> <b>ปีรถ</b>  {{item.car.cc}}</span>
+            </div>
             
-            <span class="label-item"> <b>ชื่อ</b>  {{item.customer.fistname}}</span>
-            <span class="label-item"> <b>นามสกุล</b>  {{item.customer.lastname}}</span>
-            <span class="label-item"> <b>เพศ</b>  {{item.customer.sex}}</span>
-            <span class="label-item"> <b>เลขบัตรประจำตัวประชาชน</b>  {{item.customer.idcard}}</span>
-            <span class="label-item"> <b>ที่อยู่</b>  {{item.customer.address}}</span>
+            <div v-if="item.customer">
+                <span class="label-item"> <b>ชื่อ</b>  {{item.customer.fistname}}</span>
+                <span class="label-item"> <b>นามสกุล</b>  {{item.customer.lastname}}</span>
+                <span class="label-item"> <b>เพศ</b>  {{item.customer.sex}}</span>
+                <span class="label-item"> <b>เลขบัตรประจำตัวประชาชน</b>  {{item.customer.idcard}}</span>
+                <span class="label-item"> <b>ที่อยู่</b>  {{item.customer.address}}</span>
+            </div>
 
-            <span class="label-item"> <b>วันหมดประกัน (วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.pragun}}</span>
-            <span class="label-item"> <b>วันหมดภาษี (วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.pasee}}</span>
-            <span class="label-item"> <b>วันหมดกรรมธรรม์(วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.grommatun}}</span>
+            <div v-if="item.info">
+                <span class="label-item"> <b>วันหมดประกัน (วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.pragun}}</span>
+                <span class="label-item"> <b>วันหมดภาษี (วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.pasee}}</span>
+                <span class="label-item"> <b>วันหมดกรรมธรรม์(วัน/เดือน/ปี พ.ศ.)</b>  {{item.info.grommatun}}</span>
+            </div>
             </div>
         </div>
         </div>

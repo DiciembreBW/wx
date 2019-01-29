fb634d5dc1b89e81295bb4f3ee524981ed1626c2
diciembrebw@gmail.com <diciembrebw@gmail.com>
2019-01-29 22:11:25 +0700

    ทำวิธีค้นหา item

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <!-- <items-component :data="text"/> -->
+    
+    <!-- <select-component :data="fields"></select-component> -->
+
+    <select v-model="fieldSearch">
+      <option v-for="(item, index) in fields" :key="index" :value="item.field">
+        {{item.field}}
+      </option>
+    </select>
+
+    {{fieldSearch}}
+    
+    <div class="field">
+      <label class="label"></label>
+      <div class="control">
+        <input type="text" @keyup.enter="searchItemFormFirestore($event)" class="input" placeholder="ค้นหา">
+      </div>
+    </div>
+
     <div v-for="(item, index) in text" :key="index">
       <nuxt-link :to="'item/'+item.id">  
         <item-on-index-component :data="item" />
@@ -14,7 +31,10 @@
 // import component
 import ItemsComponent from '@/components/ItemsComponent'
 import ItemOnIndexComponent from '@/components/ItemOnIndexComponent'
+import SelectComponent from '@/components/form/SelectComponent'
 import {fireDb} from '@/plugins/Firestore'
+
+import Data from '@/static/initialData.json'
 
 export default {
   
@@ -30,13 +50,23 @@ export default {
     })
 
     return {
-      text: item
+      text: item,
+      fields: Data.fields,
+      fieldSearch: ''
     }
   },
 
   components: {
     ItemsComponent,
-    ItemOnIndexComponent
+    ItemOnIndexComponent,
+    SelectComponent
+  },
+
+  methods: {
+    searchItemFormFirestore: async function (event) {
+      let value = event.target.value
+      console.log(value)
+    }
   }
   
 }
